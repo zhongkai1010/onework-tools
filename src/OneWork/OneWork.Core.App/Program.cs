ea0ec0879ddfe89458f79c7bc39c50e425cf6442
zhongkai <zhongkai1010@163.com>
2020-06-19 21:30:14 +0800

    Autofac.md

---
+++ src/OneWork/OneWork.Core.App/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Autofac;
+
+namespace OneWork.Core.App
+{
+    public interface IRepository
+    {
+        void Insert();
+    }
+
+    public class UserRepository: IRepository
+    {
+        public void Insert()
+        {
+            Console.WriteLine("Insert User");
+        }
+    }
+
+    public class RoleRepository : IRepository
+    {
+        public void Insert()
+        {
+            Console.WriteLine("Insert Role");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<RoleRepository>().As<IRepository>();
+            builder.RegisterType<UserRepository>().As<IRepository>();
+            IContainer container = builder.Build();
+            IRepository repository  = container.Resolve<IRepository>();
+            repository.Insert();
+
+        }
+    }
+}
