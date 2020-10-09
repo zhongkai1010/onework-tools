67919462ea44e2dc8ec15b3afa7873f49db06e64
zhongkai <zhongkai1010@163.com>
2020-10-09 16:16:14 +0800

    新增Java知识点

---
+++ source/demo/java/quickstart/src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ * @author zhong
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("123");
+    }
+}
+
