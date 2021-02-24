ff2dd199c7b80c1469744c209b2c542fe56f34fd
zhongkai <zhongkai1010@163.com>
2021-02-25 07:45:46 +0800

    拆分数据模型接口文件，多个接口放入独立文件

---
+++ source/onework_manage_api/app/controller/api/model/dataBehavior.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: 钟凯
+ * @Date: 2021-02-24 21:55:40
+ * @LastEditTime: 2021-02-24 21:55:59
+ * @LastEditors: 钟凯
+ * @Description:
+ * @FilePath: \onework_manage_api\app\controller\api\model\dataBehavior.js
+ * @可以输入预定的版权声明、个性签名、空行等
+ */
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class DataBehaviorController extends Controller {
+  async echo() {
+
+  }
+}
+
+module.exports = DataBehaviorController;
---
+++ source/onework_manage_api/app/controller/api/model/dataItem.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: 钟凯
+ * @Date: 2021-02-24 21:55:32
+ * @LastEditTime: 2021-02-24 21:56:05
+ * @LastEditors: 钟凯
+ * @Description:
+ * @FilePath: \onework_manage_api\app\controller\api\model\dataItem.js
+ * @可以输入预定的版权声明、个性签名、空行等
+ */
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class DataItemController extends Controller {
+  async echo() {
+
+  }
+}
+
+module.exports = DataItemController;
