5fbc8cd75eb98c88b057af524302c71471b5ef25
zhongkai <zhongkai1010@163.com>
2021-11-13 09:26:09 +0800

    调整目录

--- apps/demo/.net/net_database_schema/lib/SchemaExplorer/SchemaExplorer/#cTc/#xVc.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace #cTc
-{
-	// Token: 0x02000018 RID: 24
-	// (Invoke) Token: 0x06000160 RID: 352
-	internal delegate bool #xVc(object component);
-}
---
+++ apps/onework-net-sample/onework-donet-ef-sample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace OneWork
+{
+    class Program
+    {
+        static Task Main(string[] args)
+        {
+            using IHost host = CreateHostBuilder(args).Build();
+
+
+
+            return host.RunAsync();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args).ConfigureServices((_, services) =>
+            {
+             
+            });
+        }
+
+    }
+}
+
