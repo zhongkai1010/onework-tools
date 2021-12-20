8cbedcfab26b5e21f3dfa1433a15bfcde519a966
zhongkai <zhongkai1010@163.com>
2021-12-21 00:24:28 +0800

    添加maven案例

---
+++ apps/sample/java/onework-maven-sample/src/main/java/com/onework/mavensmple/SampleApplicationBoot.java
@@ -0,0 +1,17 @@
+package com.onework.mavensmple;
+
+/**
+ * @projectName: onework-maven-sample
+ * @package: com.onework.mavensmple
+ * @className: SampleApplicationBoot
+ * @author: 钟凯
+ * @description: 描述
+ * @date: 2021/12/20 23:22
+ * @version: 1.0
+ */
+public class SampleApplicationBoot {
+
+  public static void main(String[] args) {
+
+  }
+}
