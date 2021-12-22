b580f17e45dca2642d3ef8e9138239f5ffc10b51
zhongkai <zhongkai1010@163.com>
2021-12-22 23:03:14 +0800

    加入mybaits plus示例

---
+++ apps/sample/java/onework-mybatis-plus-sample/src/test/java/onework/samole/mybatisplussample/OneworkMybatisPlusSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package onework.samole.mybatisplussample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneworkMybatisPlusSampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
