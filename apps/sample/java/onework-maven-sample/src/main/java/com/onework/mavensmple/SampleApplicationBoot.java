c8dc0eae8620a98f72d416c5dfb5dc36db9483cd
zhongkai1010 <zhongkai1010@163.com>
2021-12-22 15:18:14 +0800

    加入mybatis的sample

--- apps/sample/java/onework-maven-sample/src/main/java/com/onework/mavensmple/SampleApplicationBoot.java
+++ apps/sample/java/onework-maven-sample/src/main/java/com/onework/mavensmple/SampleApplicationBoot.java
@@ -11,7 +11,21 @@ package com.onework.mavensmple;
  */
 public class SampleApplicationBoot {
 
-  public static void main(String[] args) {
-
-  }
+    public static void main(String[] args) {
+        System.out.println("main start...");
+        Thread t = new Thread(() -> {
+            System.out.println("thread run...");
+            try {
+                Thread.sleep(30);
+            } catch (InterruptedException e) {
+            }
+            System.out.println("thread end.");
+        });
+        t.start();
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException e) {
+        }
+        System.out.println("main end...");
+    }
 }
