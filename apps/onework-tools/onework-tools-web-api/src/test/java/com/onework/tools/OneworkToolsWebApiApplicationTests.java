db980e0b4fa8376d1c2644ee474600ecbb9cc348
zhongkai1010 <zhongkai1010@163.com>
2022-04-15 18:04:00 +0800

    加入分词功能

--- apps/onework-tools/onework-tools-web-api/src/test/java/com/onework/tools/OneworkToolsWebApiApplicationTests.java
+++ apps/onework-tools/onework-tools-web-api/src/test/java/com/onework/tools/OneworkToolsWebApiApplicationTests.java
@@ -1,26 +1,31 @@
 package com.onework.tools;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.extra.tokenizer.Result;
+import cn.hutool.extra.tokenizer.TokenizerEngine;
+import cn.hutool.extra.tokenizer.TokenizerUtil;
+import cn.hutool.extra.tokenizer.Word;
 import com.onework.tools.domain.database.DomainDatabaseException;
-import com.onework.tools.domain.translate.Language;
-import com.onework.tools.domain.translate.provide.ThreeTranslateService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Iterator;
 
 @Slf4j
 @SpringBootTest
 class OneworkToolsWebApiApplicationTests {
 
-    @Autowired
-    private ThreeTranslateService translateService;
-
     @Test
     void contextLoads() throws DomainDatabaseException {
 
-        //        TranslateResult translateResult = translateService.translateText("hello\nthree", "auto", "auto");
-        //        Assertions.assertTrue(true);
+        TokenizerEngine engine = TokenizerUtil.createEngine();
 
-        System.out.println(Language.en.toString());
+        //解析文本
+        String text = "领域模式划分项目层次结构，考虑代码生成器代码与业务代码合理";
+        Result result = engine.parse(text);
+        //输出：这 两个 方法 的 区别 在于 返回 值
+        String resultStr = CollUtil.join((Iterator<Word>)result, " ");
+        System.out.println(resultStr);
     }
 }
